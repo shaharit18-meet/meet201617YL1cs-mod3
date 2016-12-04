7e6b9e4e71a30351b24bb3853e51bf19c0be93fa
none <none>
2016-12-04 20:02:05 +0200

    setp b is done

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -1,13 +1,18 @@
+
 #This script performs some simple tests on the UserAccount class.
+
 import UserAccount
 
 #Three things are missing from the line below - fill them in
-my_user=UserAcount(  ,  ,  )
+my_user=UserAccount("shaharit18- meet", "meetyear18" , "my secret")
 
 #Call the print_secret method (function) - it takes one input - a guess for the password.
 
-#Use the wrong password as input here
-my_user.
+my_user.print_secret("meetyear18")
 
+
+#Use the wrong password as input here
+my_user.password("meetyear17")
 #Use the right password here
-my_user.
+my_user.password("meetyear18")
+
